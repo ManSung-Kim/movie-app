9283b3508ddbb833b39122d893a8a02c49421134
KIMMANSUNG <jactlab@gmail.com>
2021-03-10 22:51:59 +0900

    movie app : 1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Food extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "hello",
+    };
+  }
+
+  render() {
+    const name = this.props.name;
+    return (
+      <div>
+        <button title={name}>
+          {name}
+        </button>
+      </div>
+    );
+  }
+}
+
+// getMovies = async () => {
+//   const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");  
+// }
+
+// getMovies = async function() {
+//   const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");  
+//   return movies;
+// }
+
+
+// async getMoviesAsync() {
+//     const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Food name={getMovies.data.data.movies}/>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      movies: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Food name="testfood"/>
+      </div>
+    );
+  }
 }
 
 export default App;
