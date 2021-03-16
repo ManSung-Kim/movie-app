3f03a2257276deb4bf5d2471ea781ec20768b7ef
KIMMANSUNG <jactlab@gmail.com>
2021-03-16 16:10:39 +0900

    mediaquery : add media query

--- src/App.js
+++ src/App.js
@@ -3,26 +3,8 @@ import './App.css';
 import React from 'react';
 import axios from 'axios';
 import Movies from './Movies';
+import { useMediaQuery } from "react-responsive"
 
-class Food extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "hello",
-    };
-  }
-
-  render() {
-    const name = this.props.name;
-    return (
-      <div>
-        <button title={name}>
-          {name}
-        </button>
-      </div>
-    );
-  }
-}
 
 // getMovies = async () => {
 //   const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");  
@@ -46,15 +28,17 @@ class Food extends React.Component {
 //   );
 // }
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      movies: []
-    }
-    //this.getMovies();
-  }
+
+//class App extends React.Component {
+function App() {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     isLoading: true,
+  //     movies: []
+  //   }
+  //   //this.getMovies();
+  // }
 
   // getMovies = async() => {
   //   const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
@@ -64,20 +48,65 @@ class App extends React.Component {
   //   console.log(this.state.movies);
   // }
 
-  render() {
-    //this.getMovies();
-    // return (
-    //   <div className="App">
-    //     {/* <Food name="testfood"/> */}
-    //     <Food name={this.movies}/>
-    //   </div>
-    // );
-    return (
+  const isPc = useMediaQuery( {
+    query : "(min-width:1024px)"
+  });
+  const isTablet = useMediaQuery({
+    query : "(min-width:768px) and (max-width:1023px)"
+  });
+  const isMobile = useMediaQuery({
+    query : "(min-width:767px)"
+  });
+
+  // render() {
+  //   //this.getMovies();
+  //   // return (
+  //   //   <div className="App">
+  //   //     {/* <Food name="testfood"/> */}
+  //   //     <Food name={this.movies}/>
+  //   //   </div>
+  //   // );
+  //   return (
+  //     <div class='container'>
+  //       <div>
+  //         {isPc && <p>Pc Mode</p>}
+  //         {isTablet && <p>Tablet Mode</p>}
+  //         {isMobile && <p>Mobile Mode</p>}
+  //       </div>
+  //       <Movies />
+  //     </div>
+  //   );
+  // }
+
+  const getDeviceType = () => {
+    let dev = "Device is ";
+
+    if(isPc) {
+      dev += "PC";
+    } else if(isTablet) {
+      dev += "Tablet";
+    } else if(isMobile) {
+      dev += "Mobile";      
+    } else {
+      dev += "unknown";
+    }
+
+    return <p>{dev}</p>;
+  }
+
+  return (
+    <div>      
+      <div>
+        {getDeviceType()}
+        {/* {isPc && <p>Pc Mode</p>}
+        {isTablet && <p>Tablet Mode</p>}
+        {isMobile && <p>Mobile Mode</p>} */}
+      </div>
       <div class='container'>
         <Movies />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
